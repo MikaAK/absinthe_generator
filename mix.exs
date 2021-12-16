e505c9fbc0ec2381edb7df881b3c6c7a9ca00298
MikaAK <mika@kalathil.me>
2021-12-16 00:04:48 -0800

    chore: add LICENSE and update package info

--- mix.exs
+++ mix.exs
@@ -5,9 +5,12 @@ defmodule AbsintheGenerator.MixProject do
     [
       app: :absinthe_generator,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Generators for absinthe to help reduce writing boilerplate",
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -20,7 +23,22 @@ defmodule AbsintheGenerator.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
     [
+      maintainers: ["Mika Kalathil"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/MikaAK/absinthe_generator"},
+      files: ~w(mix.exs README.md CHANGELOG.md LICENSE lib)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "AbsintheGenerator",
+      source_url: "https://github.com/MikaAK/absinthe_generator",
     ]
   end
 end
