21f4f50b176f147eaf47598513912e47c004e0bf
MikaAK <mika@kalathil.me>
2022-03-02 03:27:56 -0700

    feat: add ability to pass options to Mix.Generator.create_file

--- lib/absinthe_generator/file_writer.ex
+++ lib/absinthe_generator/file_writer.ex
@@ -1,21 +1,9 @@
 defmodule AbsintheGenerator.FileWriter do
   @callback file_path(generation_struct :: struct) :: Path.t | String.t
 
-  def write(%generation_struct{} = data, file_contents) do
+  def write(%generation_struct{} = data, file_contents, opts) do
     file_path = generation_struct.file_path(data)
 
-    if File.exists?(file_path) do
-      inject_contents_to_existing_file(file_path, file_contents)
-    else
-      force_write_file(file_path, file_contents)
-    end
-  end
-
-  defp force_write_file(path, contents) do
-    Mix.Generator.create_file(path, contents)
-  end
-
-  def inject_contents_to_existing_file(path, contents) do
-
+    Mix.Generator.create_file(file_path, file_contents, opts)
   end
 end
